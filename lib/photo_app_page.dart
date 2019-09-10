98e8175ccaea8e34f8e81a3291ddda10b29d442a
Daiw <290121507@qq.com>
2019-09-11 00:56:23 +0800

    完成图片从相册和拍照获取功能
    完成图片从相册和拍照获取功能

--- lib/photo_app_page.dart
+++ lib/photo_app_page.dart
@@ -16,13 +16,17 @@ class PhotoApp extends StatefulWidget {
 class _PhotoAppState extends State<PhotoApp> {
   String title = '拍照APP开发-图片获取与图片展示';
 
-  File _image;
+  List<File> _images = [];
 
-  Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+  Future getImage(bool isTakePhoto) async {
+    Navigator.pop(context);
+    var image = await ImagePicker.pickImage(
+        source: isTakePhoto ? ImageSource.camera : ImageSource.gallery);
 
     setState(() {
-      _image = image;
+      if(image != null) {
+        _images.add(image);
+      }
     });
   }
 
@@ -41,17 +45,80 @@ class _PhotoAppState extends State<PhotoApp> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-            onPressed: getImage,
+            onPressed: () =>  _showSelect(),
             tooltip: 'Pick image',
             child: Icon(
               Icons.add_a_photo,
             )),
-        body: Wrap(
-          children: <Widget>[
-              Image.file(_image != null ? _image : File('images/icon_light.pn'))
-          ],
+        body: Center(
+          child: Wrap(
+            spacing: 5,
+            runSpacing: 5,
+            children: _imagePicks(),
+          ),
         ),
       ),
     );
+  }
+
+  _showSelect() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => Container(
+              height: 120,
+              child: Column(
+                children: <Widget>[
+                  item('拍照', true),
+                  item('从相册获取', false),
+                ],
+              ),
+            ));
+  }
+
+  item(String title, bool isTakePhoto) {
+    return GestureDetector(
+      child: ListTile(
+        leading: Icon(isTakePhoto ? Icons.camera_alt : Icons.photo_library),
+        title: Text(title),
+        onTap: () => getImage(isTakePhoto),
+      ),
+    );
+  }
+
+  _imagePicks() {
+    return _images.map((file) {
+      return Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5),
+            child: Image.file(
+              file,
+              width: 120,
+              height: 90,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Positioned(
+            top: 5,
+            right: 5,
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _images.remove(file);
+                  });
+                },
+                child: ClipOval(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.black54),
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: Icon(Icons.close),
+                    ),
+                  ),
+                )),
+          )
+        ],
+      );
+    }).toList();
   }
 }
